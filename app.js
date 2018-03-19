9436a22566339b7c60ced20a94f450d6cd066d1a
Alejandro García <alejogs4@gmail.com>
2018-03-19 14:53:56 -0500

    Agrega controladores y modelos para notas y profesores

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const bodyParser = require('body-parser')
 const testsRoutes = require('./routes/testsRoutes')
 const questionsRoutes = require('./routes/questionsRoutes')
 const studentsRoutes = require('./routes/studentsRoutes')
+const notesRoutes = require('./routes/notesRoutes')
+const teacherRoutes = require('./routes/teachersRoutes')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended : false }))
+
 app.use(testsRoutes)
 app.use(questionsRoutes)
 app.use(studentsRoutes)
+app.use(notesRoutes)
+app.use(teacherRoutes)
+
 
 module.exports = app
